c34f00f0afe201708d823d5a6175bdc61b07aba1
Alaminplabon <donplabon94@gmail.com>
2023-07-20 09:49:31 +0600

    Seed Method

--- src/Services/Catalog.API/Models/Product.cs
+++ src/Services/Catalog.API/Models/Product.cs
@@ -9,7 +9,7 @@ namespace Catalog.API.Models
         public string Name { get; set; }
         public string Description { get; set; }
         public string Category { get; set; }
-        public string summary { get; set; }
+        public string Summary { get; set; }
         public string ImageFile { get; set; }
         public double Price { get; set; }
     }
